fe43a4e703f72d1aadd2ad3091ce0ba77a1f5a29
Christopher Allan Webber <cwebber@dustycloud.org>
2012-02-14 11:49:25 -0600

    Added a basic setup.py to the CC legalcode eratta tool

---
+++ setup.py
@@ -0,0 +1,37 @@
+# CC Legalcode Eratta Tool
+# 
+# Written in 2012 by Christopher Allan Webber, Jonathan Palecek,
+#   Creative Commons
+# 
+# To the extent possible under law, the author(s) have dedicated all
+# copyright and related and neighboring rights to this software to the
+# public domain worldwide. This software is distributed without any
+# warranty.
+# 
+# You should have received a copy of the CC0 Public Domain Dedication along
+# with this software. If not, see
+# <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+from setuptools import setup, find_packages
+
+setup(
+    name="cc.legaleratta",
+    version="0.1-dev",
+    namespace_packages=['cc',],
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    zip_safe=False,
+    include_package_data = True,
+
+    # scripts and dependencies
+    install_requires = [
+        'PasteScript',
+        ],
+
+    # author metadata
+    author = 'Jonathan Palecek',
+    author_email = 'jonathan@creativecommons.org',
+    description = '',
+    # I don't think pypi accepts this yet :)
+    #license = 'CC0',
+    url = 'http://creativecommons.org',
+    )
