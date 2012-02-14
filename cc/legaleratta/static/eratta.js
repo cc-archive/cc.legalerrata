14e7618e96a22c08f60050ea613b30afba62c5a8
Christopher Allan Webber <cwebber@dustycloud.org>
2012-02-14 17:32:38 -0600

    Putting eratta.js and json stuff in the right place

---
+++ cc/legaleratta/static/eratta.js
@@ -0,0 +1,13 @@
+/* Creative Commons legal eratta annotation tool 
+
+   Written in 2012 by Jonathan Palecek, Creative Commons
+
+   To the extent possible under law, the author(s) have dedicated all
+   copyright and related and neighboring rights to this software to
+   the public domain worldwide. This software is distributed without
+   any warranty.
+
+   You should have received a copy of the CC0 Public Domain Dedication
+   along with this software. If not, see
+   <http://creativecommons.org/publicdomain/zero/1.0/>.
+*/
